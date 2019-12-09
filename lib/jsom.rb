14233a7ac27878611e77a15706c0cc6c6e129002
Mateusz Jarosz <mateusz@useo.pl>
2019-12-09 18:38:03 +0100

    Init repository

---
+++ lib/jsom.rb
@@ -0,0 +1,6 @@
+require "jsom/version"
+
+module JSOM
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jsom_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe JSOM do
+  it "has a version number" do
+    expect(JSOM::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
